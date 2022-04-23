4fc50b7875539613df0089f9bbae8e8fdfe56136
Anthony Connell <toco85@gmail.com>
2022-04-23 09:35:59 -0400

    Finally Finished

--- public/game.js
+++ public/game.js
@@ -8,7 +8,7 @@ const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
   if (gameOver) {
-    if (confirm('You lost. Press ok to restart.')) {
+    if (confirm('Game Over. Press ok to restart.')) {
       window.location = '/'
     }
     return
